f74e1aa3da23a26ecfd8f588c01a8c3272c82aaf
stephen <osstephen70@gmail.com>
2025-01-15 19:36:51 +0100

    feat/ added the hacker room and implemented the sclae, position and rotation

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -47,9 +47,11 @@ function Navbar() {
           </nav>
         </div>
       </div>
-      <div
-        className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'} `}
-      ></div>
+      <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'} `}>
+        <nav className='p-5'>
+          <NavItems />
+        </nav>
+      </div>
     </header>
   );
 }
